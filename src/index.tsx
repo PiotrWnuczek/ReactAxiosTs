31b617728c3bbefd3a2ba32b6cb35969cbed579c
Piotr Wnuczek <piotrantoniwnuczek@gmail.com>
2022-06-24 13:33:55 +0200

    basic pattern

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import reducer from 'logic/reducer';
+import thunk from 'redux-thunk';
+import App from 'App';
+import 'index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const persistConfig = { key: 'ReactAxiosTs', storage };
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = createStore(persistedReducer, compose(applyMiddleware(thunk)));
+const persistor = persistStore(store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
